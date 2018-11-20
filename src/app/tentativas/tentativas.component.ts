ed455ddf026b0a15f9042631ab3f12a0f461d757
Divino Miranda <divinomiranda@yahoo.com.br>
2018-11-20 18:02:07 -0200

    Aula 68

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,3 +1,4 @@
+import { Coracao } from './../shared/coracao.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,7 +11,15 @@ export class TentativasComponent implements OnInit {
   public coracaoVazio: string = '/assets/coracao_vazio.png';
   public coracaoCheio: string = '/assets/coracao_cheio.png';
   
-  constructor() { }
+  public coracoes: Coracao[] = [
+    new Coracao(true),
+    new Coracao(true),
+    new Coracao(true)
+  ]
+
+  constructor() {
+    console.log(this.coracoes)
+   }
 
   ngOnInit() {
   }
